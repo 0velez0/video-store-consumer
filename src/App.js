9fedb326b58ef2121d785c96037b5d1d7d476ed6
0velez0 <mariaivelez@gmail.com>
2018-06-18 15:49:48 -0700

    Modify App.js to use 'react-router-dom' to build basic structure of SPA

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+      const Home = () => (
+        <div>
+          <h2>Home</h2>
+        </div>
+      )
+
+      const Search = () => (
+        <div>
+          <h2>Search</h2>
+        </div>
+      )
+
+      const Library = () => (
+        <div>
+          <h2>Library</h2>
+        </div>
+      )
+
+      const Customers = () => (
+        <div>
+          <h2>Customers</h2>
+        </div>
+      )
+
+      const App = () => (
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/search">Search</Link></li>
+              <li><Link to="/library">Library</Link></li>
+              <li><Link to="/customers">Customers</Link></li>
+            </ul>
+
+            <hr/>
+            
+            <Route exact path="/" component={Home}/>
+            <Route path="/search" component={Search}/>
+            <Route path="/library" component={Library}/>
+            <Route path="/customers" component={Customers}/>
+
+          </div>
+        </Router>
+      )
+
 
 export default App;
