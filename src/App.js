38478fa2fb4d9740b488c7613b493e5d7bc8cd64
Dikla <dikla.aharoni@gmail.com>
2018-06-18 22:22:26 -0700

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import Library from "./components/Library";
+import Customers from "./components/Customers";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter>
+      <div>
+          <h1>VIDEO STORE</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/search">Search</NavLink></li>
+            <li><NavLink to="/library">Library</NavLink></li>
+            <li><NavLink to="/customers">Customers</NavLink></li>
+          </ul>
+          <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route path="/search" component={Search}/>
+          <Route path="/library" component={Library}/>
+          <Route path="/customers" component={Customers}/>
+          </div>
+        </div>
+        </HashRouter>
     );
   }
 }
