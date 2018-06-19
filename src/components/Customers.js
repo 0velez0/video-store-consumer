75a6be041a527f75d7c481894e68f3c054368d57
Dikla <dikla.aharoni@gmail.com>
2018-06-18 22:23:42 -0700

    Added customers component

---
+++ src/components/Customers.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Customers extends Component {
+  static propTypes = {
+
+  };
+
+
+  render() {
+
+    return (
+      <div>
+        <h2>Customers</h2>
+
+      </div>
+    )
+  }
+
+}
+
+
+export default Customers;
