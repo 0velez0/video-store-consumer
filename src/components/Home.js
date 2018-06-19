691ac2ff7ebbe3811c49fc0dcc3a4aed64d44773
Dikla <dikla.aharoni@gmail.com>
2018-06-18 22:23:05 -0700

    Added home component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Home extends Component {
+  static propTypes = {
+
+  };
+
+
+  render() {
+
+    return (
+      <div>
+        <h2>Home</h2>
+
+      </div>
+    );
+  }
+
+}
+
+
+export default Home;
